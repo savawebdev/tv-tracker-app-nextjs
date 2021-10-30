443f686dd035c8ac1585ad25fb3bc7b53c82eb71
savawebdev <92175087+savawebdev@users.noreply.github.com>
2021-10-30 22:40:34 +0300

    issue#1 created navbar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Header.module.scss';
 import Brand from './Brand/Brand';
+import Navbar from './Navbar/Navbar';
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <Brand />
+      <Navbar />
     </header>
   );
 };
