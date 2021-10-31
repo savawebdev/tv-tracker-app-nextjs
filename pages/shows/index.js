ebcf79f34e5f8da95ae1f99c1c526b08d4a4889b
savawebdev <92175087+savawebdev@users.noreply.github.com>
2021-10-31 17:14:26 +0200

    issue#5 finished shows page

--- pages/shows/index.js
+++ pages/shows/index.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Shows from '../../components/ShowsPage/Shows';
 
-const ShowsPage = ({ topRated }) => {
+const ShowsPage = ({ topRated, airingTonight, trendingShows }) => {
   return (
     <Fragment>
-      <Shows topRated={topRated} />
+      <Shows
+        topRated={topRated}
+        airingTonight={airingTonight}
+        trendingShows={trendingShows}
+      />
     </Fragment>
   );
 };
@@ -12,16 +16,30 @@ const ShowsPage = ({ topRated }) => {
 export const getStaticProps = async () => {
   try {
     const apiKey = process.env.TMDB_API_KEY;
+
+    // Fetch Top Rated Shows
     const result = await fetch(
       `https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}&language=en-US&page=1`
     );
     const topRated = await result.json();
 
-    console.log(topRated);
+    // Fetch Airing Tonight Shows
+    const airingResult = await fetch(
+      `https://api.themoviedb.org/3/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`
+    );
+    const airingTonight = await airingResult.json();
+
+    // Fetch Trending Shows
+    const trendingResult = await fetch(
+      `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`
+    );
+    const trendingShows = await trendingResult.json();
 
     return {
       props: {
         topRated: topRated.results,
+        airingTonight: airingTonight.results,
+        trendingShows: trendingShows.results,
       },
     };
   } catch (error) {
